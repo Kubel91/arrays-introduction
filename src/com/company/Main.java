67c2b05a364ba848c485d08a9b2c03da1230daee
Kubel91 <michaelrosenfeld@outlook.de>
2021-02-07 12:02:50 +0100

    test commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] myIntArray = {1,2,3,4,5,6,7,8,9,10};
+	myIntArray [5] = 50;
+	myIntArray [4] = 43;
+	myIntArray [3] = 234;
+	double[] myDoubleArray = new double[10];
+	System.out.println(myIntArray[5]);
+    }
+}
